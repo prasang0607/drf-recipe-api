7cff443927ee3af3f9e037f062e52f9d5c859ba3
Prasang Singhal <prasang.singhal@gmail.com>
2022-03-13 21:42:44 +0530

    Added endpoints for creating and authenticating users

--- drf_recipe/drf_recipe/settings.py
+++ drf_recipe/drf_recipe/settings.py
@@ -39,7 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core'
+    # 3rd party apps
+    'rest_framework',
+    'rest_framework.authtoken',
+    # our apps
+    'core',
+    'user',
 ]
 
 MIDDLEWARE = [
---
+++ drf_recipe/user/tests/test_user_api.py
@@ -0,0 +1,119 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from rest_framework.test import APIClient
+from rest_framework import status
+
+CREATE_USER_URL = reverse('user:create')
+TOKEN_URL = reverse('user:token')
+
+
+def create_user(**kwargs):
+    return get_user_model().objects.create_user(**kwargs)
+
+
+class PublicUserApiTests(TestCase):
+    """ Tests the users API (public) """
+
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_create_valid_user_success(self):
+        """ Test creating user with valid payload is successful """
+        payload = {
+            'email': 'test@gmail.com',
+            'password': 'TestPass123',
+            'name': 'Test name'
+        }
+
+        res = self.client.post(CREATE_USER_URL, payload)
+
+        # check status code
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+
+        user = get_user_model().objects.get(**res.data)
+
+        # check password
+        self.assertTrue(user.check_password(payload['password']))
+        # check password field is not in response
+        self.assertNotIn('password', res.data)
+
+    def test_user_exists(self):
+        """ Test creating a user that already exists """
+        payload = {
+            'email': 'test@outlook.com',
+            'password': 'TestPass123',
+            'name': 'Test name'
+        }
+
+        create_user(**payload)
+
+        res = self.client.post(CREATE_USER_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_password_too_short(self):
+        """ Test that password must be more than 5 characters """
+        payload = {
+            'email': 'test@hotmail.com',
+            'password': 'pw',
+            'name': 'Test name'
+        }
+
+        res = self.client.post(CREATE_USER_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+        user_exists = get_user_model().objects. \
+            filter(email=payload['email']).exists()
+
+        self.assertFalse(user_exists)
+
+    def test_create_token_for_user(self):
+        """ Test that a token is created for the user """
+        payload = {
+            'email': 'test@gmail.com',
+            'password': 'TestPass'
+        }
+
+        create_user(**payload)
+
+        res = self.client.post(TOKEN_URL, payload)
+
+        self.assertIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    def test_create_token_invalid_creds(self):
+        """ Test that token is not created if invalid creds are given """
+        payload = {
+            'email': 'test@gmail.com',
+            'password': 'TestPass'
+        }
+        create_user(**payload)
+
+        payload['password'] = 'NotAValidPass'
+
+        res = self.client.post(TOKEN_URL, payload)
+
+        self.assertNotIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_token_no_user(self):
+        """ Test that token is not created if user doesn't exist """
+        payload = {
+            'email': 'test@gmail.co.uk',
+            'password': 'TestPass'
+        }
+
+        res = self.client.post(TOKEN_URL, payload)
+
+        self.assertNotIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_token_missing_field(self):
+        """ Test that email and password are required """
+        res = self.client.post(TOKEN_URL,
+                               {'email': 'one', 'password': ''})
+
+        self.assertNotIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
