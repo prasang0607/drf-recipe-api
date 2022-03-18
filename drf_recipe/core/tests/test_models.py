0f7078e3b7cf95c0f87e076486a3e549eb00c131
Prasang Singhal <prasang.singhal@gmail.com>
2022-03-18 22:59:20 +0530

    Added recipe model.

--- drf_recipe/core/tests/test_models.py
+++ drf_recipe/core/tests/test_models.py
@@ -53,10 +53,21 @@ class ModelTests(TestCase):
         self.assertEqual(tag.name, str(tag))
 
     def test_ingredient_str(self):
-        """ Test the ingredients string representation """
+        """ Test the ingredient string representation """
         ingredient = models.Ingredient.objects.create(
             user=sample_user(),
             name='Cucumber'
         )
 
         self.assertEqual(str(ingredient), ingredient.name)
+
+    def test_recipe_str(self):
+        """ Test the recipe string representation """
+        recipe = models.Recipe.objects.create(
+            user=sample_user(),
+            title='Steak and Mushroom Sauce',
+            time_minutes=5,
+            price=5.00
+        )
+
+        self.assertEqual(str(recipe), recipe.title)
