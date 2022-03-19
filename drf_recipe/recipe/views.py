16b7ab2b30683a15f8404fb78f3bea32256e342e
Prasang Singhal <prasang.singhal@gmail.com>
2022-03-19 19:10:02 +0530

    Added feature to filter recipes.

--- drf_recipe/recipe/views.py
+++ drf_recipe/recipe/views.py
@@ -1,4 +1,3 @@
-import rest_framework
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework import viewsets, mixins, status
@@ -43,10 +42,25 @@ class RecipeViewSet(viewsets.ModelViewSet):
     authentication_classes = (TokenAuthentication,)
     queryset = Recipe.objects.all()
 
+    def _params_to_ints(self, qs: str):
+        """ Convert a list of string IDs to a list of integers """
+        return [int(str_id) for str_id in qs.split(',')]
+
     def get_queryset(self):
         """ Retrieve recipes for authenticated user """
-        return self.queryset.filter(user=self.request.user). \
-            order_by('id')
+        tags = self.request.query_params.get('tags')
+        ingredients = self.request.query_params.get('ingredients')
+        queryset = self.queryset.filter(user=self.request.user)
+
+        if tags:
+            tag_ids = self._params_to_ints(tags)
+            queryset = queryset.filter(tags__id__in=tag_ids)
+
+        if ingredients:
+            ingredient_ids = self._params_to_ints(ingredients)
+            queryset = queryset.filter(ingredients__id__in=ingredient_ids)
+
+        return queryset.distinct().order_by('id')
 
     def get_serializer_class(self):
         """ Return appropriate serializer class """
